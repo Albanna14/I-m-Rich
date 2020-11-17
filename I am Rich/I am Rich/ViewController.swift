2c3ba7d57b3113adfc4873df51d54b5afa7be365
Muhammad Azzam Albanna <74603960+Albanna14@users.noreply.github.com>
2020-11-17 21:10:20 +0700

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Muhammad Azzam Al Bana on 17/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
